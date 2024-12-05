a31e950f9999888bf11bd0c856ebf4bc07d33c3c
DevGabriel <victorgabrieldelimarocha@gmail.com>
2024-12-05 12:02:51 -0300

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,12 +3,14 @@ function altera_color() {
     const paragrafo = document.querySelector('p');
     const corpo = document.querySelector('body');
     const botao = document.querySelector('button');
+    const botao_padrao = document.getElementById('button_padrao')
 
     cabecalho.style.color = 'blue';
     paragrafo.style.color = 'blue';
     corpo.style.backgroundColor = 'lightblue';
-    botao.style.color ='red';
+    botao.style.color ='white';
     botao.style.backgroundColor ='#dc143c';
+    botao_padrao.style.backgroundColor = '#dc143c'
 
 
 }
@@ -18,12 +20,14 @@ function padrao_color() {
     const paragrafo = document.querySelector('p');
     const corpo = document.querySelector('body');
     const botao = document.querySelector('button');
+    const botao_padrao = document.getElementById('button_padrao')
 
     cabecalho.style.color = 'white';
     paragrafo.style.color = 'white';
     corpo.style.backgroundColor = '#6e6ec1';
     botao.style.color ='white'
     botao.style.backgroundColor ='#6495ed';
+    botao_padrao.style.backgroundColor = '#6495ed'
 }
 
 window.confirm('Atividade da última aula de Autoria Web, atividade livre de Js');
